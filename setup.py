9f86ab997e9fe82b79595f8a7eab829379004e25
S.F. Schouten <stefanschouten10@gmail.com>
2019-02-17 11:14:01 +0100

    removed src added new dist

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from distutils.core import setup
 
 setup(
-        name='IRL-Imitation',
-        version='0.2dev',
-        packages=['src',],
+        name='IRL_Imitation',
+        version='0.1b',
+        packages=['irl_imitation','irl_imitation/mdp'],
         license='MIT License',
 )
 
